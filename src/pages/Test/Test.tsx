85fd9e14257865273c36f24754c2b04cc12a1f49
skociuba <kociuba.sebastian@gmail.com>
2023-03-27 17:54:43 +0200

    crate typescript display components

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,15 +1,13 @@
 import React, {useEffect} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
-import PropTypes from 'prop-types';
 
 import {ErrorMessage} from '../../components/Modules/ErrorMessage/index';
-import {FallbackIndicator} from '../../components/Packages/FallbackIndicator/index';
 
 import {fetchTestData} from './actions';
 import {testDataSelector, testLoadingSelector, errorSelector} from './selectors';
 import {contentContainer} from './Test.style';
 
-const Test = () => {
+const Test: React.FC = () => {
   const dispatch = useDispatch();
 
   const testData = useSelector((state) => testDataSelector(state));
@@ -29,12 +27,12 @@ const Test = () => {
       {testData?.length > 0 &&
         testData.map((user) => (
           <div key={user._id}>
-            <FallbackIndicator>{user.name}</FallbackIndicator>
-            <FallbackIndicator>{user.trips}</FallbackIndicator>
+            <p>{user.name}</p>
+            <p>{user.trips}</p>
             {user?.airline?.map((item) => (
               <p key={item.id}>
-                <FallbackIndicator>{item.name}</FallbackIndicator>
-                <FallbackIndicator>{item.head_quaters}</FallbackIndicator>
+                <p>{item.name}</p>
+                <p>{item.head_quaters}</p>
               </p>
             ))}
           </div>
@@ -43,7 +41,5 @@ const Test = () => {
   );
   return <div className={contentContainer}>{componentContent}</div>;
 };
-Test.propTypes = {
-  fetchTestData: PropTypes.func,
-};
+
 export default Test;
